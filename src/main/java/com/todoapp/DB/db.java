1f2d5d8f3cb4311a6d86838fccaea4ef0f6e9f41
Aaryan <aaryan14032006@gmail.com>
2021-08-25 13:58:34 +0530

    Added DB

---
+++ src/main/java/com/todoapp/DB/db.java
@@ -0,0 +1,32 @@
+package com.todoapp.DB;
+
+import java.util.Iterator;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+
+public class db {
+    MongoClient mongoClient = new MongoClient("localhost", 27017);
+    MongoDatabase database = mongoClient.getDatabase("tododb");
+    MongoCollection<Document> collection = database.getCollection("todo");
+
+    public void add(String key, String value) {
+        collection.insertOne(new Document(key, value));
+    }
+
+    public String[] getAllTheDocuments() {
+        Iterator it = collection.find().iterator();
+        int counter = 0;
+        String[] arr = new String[100];
+        while(it.hasNext()) {
+            Document doc = (Document) it.next();
+            String s = doc.getString("todo");
+            arr[counter] = s;
+            counter++;
+        }
+        return arr;
+    }
+}
